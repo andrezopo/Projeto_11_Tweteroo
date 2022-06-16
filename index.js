5b8203bfb71361bdffbd0fecc554074c65269e52
André Zopolato <andrezopo@gmail.com>
2022-06-16 00:02:11 -0300

    requisitos básicos concluídos

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+const users = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  const tweet = req.body;
+  tweets.push(tweet);
+  res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  const lastTweets = [];
+  const newTweets = tweets;
+  newTweets.forEach((tweet) => {
+    const image = users.find(() => tweet.username);
+    tweet.avatar = image.avatar;
+  });
+  if (newTweets.length > 10) {
+    for (let i = newTweets.length - 1; i > newTweets.length - 11; i--) {
+      lastTweets.push(newTweets[i]);
+    }
+    res.send(lastTweets);
+  } else {
+    res.send(newTweets);
+  }
+});
+
+app.listen(5000);
